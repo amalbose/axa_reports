40b1f54e6122be298e760bd2be2a9626f8e5f1ad
Amal Bose <axatrikx@gmail.com>
2016-07-25 19:34:18 +0530

    added ExecutionTS table

--- executions/admin.py
+++ executions/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Execution
-from .models import ExecutionTC
+from executions.models import Execution
+from executions.models import ExecutionTC
+from executions.models import ExecutionTS
 
 admin.site.register(Execution)
 admin.site.register(ExecutionTC)
+admin.site.register(ExecutionTS)
--- executions/models.py
+++ executions/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 from testcases.models import TestCase
+from testscripts.models import TestScript
 
 # Create your models here.
+
+PASS = 'PASS'
+FAIL = 'FAIL'
+NORUN = 'NO RUN'
+SKIP = 'SKIP'
+STATUS_CHOICES = (
+    (PASS, 'Pass'),
+    (FAIL, 'Fail'),
+    (NORUN, 'No Run'),
+    (SKIP, 'Skip'),
+)
+    
+    
 class Execution(models.Model):
     exec_id         = models.AutoField(primary_key=True)
     env             = models.CharField(max_length=500,blank=True, null=True)
@@ -11,23 +25,23 @@ class Execution(models.Model):
     def __str__(self):
         return str(self.exec_id);
     
+class ExecutionTS(models.Model):
+    exec_ts_id       = models.AutoField(primary_key=True)
+    script_id        = models.ForeignKey(TestScript)
+    exec_id          = models.ForeignKey(Execution)
+    status           = models.CharField(max_length=6, choices=STATUS_CHOICES, default=NORUN);
+    executed_time    = models.DateTimeField(auto_now_add=True, auto_now=False)
+    modified_time    = models.DateTimeField(auto_now_add=False, auto_now=True)
+    def __str__(self):
+        return str(self.exec_ts_id);
+
 class ExecutionTC(models.Model):
     exec_tc_id       = models.AutoField(primary_key=True)
     tc_id            = models.ForeignKey(TestCase)
-    exec_id          = models.ForeignKey(Execution)
-
-    PASS = 'PASS'
-    FAIL = 'FAIL'
-    NORUN = 'NO RUN'
-    SKIP = 'SKIP'
-    STATUS_CHOICES = (
-        (PASS, 'Pass'),
-        (FAIL, 'Fail'),
-        (NORUN, 'No Run'),
-        (SKIP, 'Skip'),
-    )
-    status          = models.CharField(max_length=6, choices=STATUS_CHOICES, default=NORUN);
+    exec_ts_id       = models.ForeignKey(ExecutionTS)
+    status           = models.CharField(max_length=6, choices=STATUS_CHOICES, default=NORUN);
     executed_time    = models.DateTimeField(auto_now_add=True, auto_now=False)
-    modified_time   = models.DateTimeField(auto_now_add=False, auto_now=True)
+    modified_time    = models.DateTimeField(auto_now_add=False, auto_now=True)
     def __str__(self):
         return str(self.exec_tc_id);
+    
