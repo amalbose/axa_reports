3fc170aed07453a5be77098a3a1f2cae405da982
Amal Bose <axatrikx@gmail.com>
2016-07-25 17:07:35 +0530

    added script path field

--- testscripts/models.py
+++ testscripts/models.py
@@ -7,6 +7,7 @@ class TestScript(models.Model):
     script_id       = models.AutoField(primary_key=True)
     plan_id         = models.ForeignKey(TestPlan)
     script_name     = models.CharField(max_length=200)
+    script_path     = models.CharField(max_length=2000, blank=True, null=True)
     owner           = models.CharField(max_length=200, blank=True, null=True)
     created_time    = models.DateTimeField(auto_now_add=True, auto_now=False)
     modified_time   = models.DateTimeField(auto_now_add=False, auto_now=True)
